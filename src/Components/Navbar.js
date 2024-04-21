76204a301bada6cb2045795ca4b481da3e5b3dbd
Soumya <soumyashru11@gmail.com>
2024-04-21 18:55:16 +0530

    new changes

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = () => {
                   aria-current="page"
                   to="/entertainment"
                 >
-                  Entertainement
+                  Entertainment
                 </Link>
               </li>
               <li className="nav-item"></li>
--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -1,12 +1,18 @@
 import React from "react";
-import loading from "./loading.gif";
+import loading from "./spinner.gif";
 
 const Spinner = () => {
+  const spinnerStyle = {
+    width: "50px",
+    height: "50px",
+  };
+
   return (
     <div className="text-center">
-      <img src={loading} alt="loading" />
+      <img src={loading} alt="loading" style={spinnerStyle}/>
     </div>
   );
 };
 
 export default Spinner;
+
